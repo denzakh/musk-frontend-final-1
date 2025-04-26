c5876ca44d65c11e4af7426424f9f321a736c4ac
Den Zakh <den.zakh@gmail.com>
2025-04-26 12:45:36 +0300

    animation

--- src/pages/Favorites.tsx
+++ src/pages/Favorites.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 import { Article } from '../types/news';
 import { getFavorites, removeFromFavorites } from '../utils/localStorage';
 import NewsCard from '../components/NewsCard';
+import { motion, AnimatePresence } from 'framer-motion';
 
 const Favorites = () => {
     const [favorites, setFavorites] = useState<Article[]>([]);
@@ -12,25 +13,42 @@ const Favorites = () => {
 
     const handleRemove = (url: string) => {
         removeFromFavorites(url);
-        setFavorites(getFavorites());
+        setFavorites((prevFavorites) =>
+            prevFavorites.filter((article) => article.url !== url)
+        );
     };
 
     return (
         <div className='p-4 grid gap-4 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3'>
-            {favorites.length > 0 ? (
-                favorites.map((article, idx) => (
-                    <NewsCard
-                        key={idx}
-                        article={article}
-                        onSave={() => handleRemove(article.url)}
-                        isFavorite
-                    />
-                ))
-            ) : (
-                <p className='text-center col-span-full'>
-                    Нет избранных новостей
-                </p>
-            )}
+            <AnimatePresence>
+                {favorites.length > 0 ? (
+                    favorites.map((article, index) => (
+                        <motion.div
+                            key={article.url}
+                            initial={{ opacity: 0, y: 20 }}
+                            animate={{ opacity: 1, y: 0 }}
+                            exit={{ opacity: 0, y: 20 }}
+                            transition={{ duration: 0.4, delay: index * 0.1 }}
+                        >
+                            <NewsCard
+                                article={article}
+                                onSave={() => handleRemove(article.url)}
+                                isFavorite
+                            />
+                        </motion.div>
+                    ))
+                ) : (
+                    <motion.p
+                        className='text-center col-span-full'
+                        initial={{ opacity: 0 }}
+                        animate={{ opacity: 1 }}
+                        exit={{ opacity: 0 }}
+                    >
+                        No hay noticias destacadas. Agrega algunas a tus
+                        favoritos para verlas aquí.
+                    </motion.p>
+                )}
+            </AnimatePresence>
         </div>
     );
 };
